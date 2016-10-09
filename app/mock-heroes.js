598247e560d4e24b94248d46960af4f7b42f126b
Fabio Albertin <albertin@gmail.com>
2016-10-09 03:50:27 +0100

    Updated ignore file

---
+++ app/mock-heroes.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.HEROES = [
+    { id: 11, name: 'Mr. Vice' },
+    { id: 12, name: 'Barco' },
+    { id: 13, name: 'Lombasto' },
+    { id: 14, name: 'Celebrittas' },
+    { id: 15, name: 'Mangenta' },
+    { id: 16, name: 'PolymerMan' },
+    { id: 17, name: 'Dynamos' },
+    { id: 18, name: 'Dr English' },
+    { id: 19, name: 'Magman' },
+    { id: 20, name: 'Brushnado' }
+];
+//# sourceMappingURL=mock-heroes.js.map
