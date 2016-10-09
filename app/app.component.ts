3f133d24e16b6c8a6fe7c5debf7f444d6ddcda6e
Fabio Albertin <albertin@gmail.com>
2016-10-09 04:05:53 +0100

    Updated app component to final

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My first Angular 2 app</h1>'
+  template: '<h1>Quickstart Angular App (plain)</h1>'
 })
 
 export class AppComponent { }
