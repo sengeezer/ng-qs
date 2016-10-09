97f47c916fc29e3575c59d8a58ebe310e472a9c1
Fabio Albertin <albertin@gmail.com>
2016-10-09 03:48:18 +0100

    Adding uncommitted changes

---
+++ app/heroes.component.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'my-heroes',
+  template:`
+  <h2>My Heroes</h2>
+  <ul class="heroes">
+    <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
+      <span class="badge">{{hero.id}}</span> {{hero.name}}
+    </li>
+  </ul>
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `,
+  styles: [`
+  .selected {
+    background-color: #cfd8dc !important;
+    color: #fff;
+  }
+  .heroes {
+    margin: 0 0 2em;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #eee;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #bbd8dc !important;
+    color: #fff;
+  }
+  .heroes li:hover {
+    color: #607d8b;
+    background-color: #ddd;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: #fff;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607d8b;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`],
+  providers: [HeroService]
+})
+
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+  constructor(private heroService: HeroService) { }
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+}
